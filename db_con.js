8f9e50dab07bf6579ce6a163b912a545a966e4d7
sayyam gatagat <sayyamg24@gmail.com>
2020-11-23 17:08:17 +0530

    added login

--- db_con.js
+++ db_con.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql')
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const md5 = require('md5')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').renderFile); // register .html as an engine in express view system
+/* defining database */
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -11,26 +15,36 @@ var con = mysql.createConnection({
     database: "insurance",
     insecureAuth : true
   });
-  
 
+  /* To start from the landing page */
+  app.use(express.static('views'));
+  
+  /* Starting database connection */
   con.connect();
+
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+  /* call login */
+  app.get('/login',(req,res)=>{
+    res.render(__dirname + '/views/login.ejs')
+  })
 
-  app.get('/index',(req,res)=>{
-    con.query('SELECT *  from agent', function (error, results, fields) {
-      if (error) throw error;
-      else{
-        console.log(results);
-        res.render("/views/index", { results, error: undefined });
-      }
-    });
-  });
+  /*login validation*/
+
+  app.post('/loguser',(req,res)=>{
+    var userid = req.body.userid;
   
+    var quer = 'select * from cred where username =' + userid;
+    console.log(userid);
+
+  })
 
   app.listen(port, () => {
       console.log(`App started at http://localhost:${port}`)
   })
   
+
+
+  /* ending database connection */
   con.end();
