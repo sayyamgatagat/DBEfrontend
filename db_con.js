c9a3e3b9f2ec237b73a7f7405d3b9f7c9f0dfe55
sayyam gatagat <sayyamg24@gmail.com>
2021-01-09 23:29:39 +0530

    Added policies and made some UI changes

--- db_con.js
+++ db_con.js
@@ -11,6 +11,53 @@ let policyid;
 let policytype;
 let pdetails;
 
+let policyTypes = [
+  {
+    pname : "Care Freedom",
+    id : 0,
+    img : './src/care.png',
+    type : 'General insurance',
+    desc: 'Insure now and get coverage of upto 3,00,000. Policy does not cover against existing health issues'
+  },
+  {
+    pname : "Heart Buddy",
+    id : 1,
+    img : './src/heartbuddy.png',
+    type : 'For Heart patients',
+    desc: 'If you are a heart patient, this policy is for you. Get coverage of upto 7,00,000'
+  },
+  {
+    pname : "Assure Diamond",
+    id : 2,
+    img : './src/diamond.png',
+    type : 'Higher cover amount',
+    desc: 'Get coverage of upto 12,00,000. Claims settled in 4 days.'
+  },
+  {
+    pname : "Health Guard",
+    id : 3,
+    img : './src/healthguard.webp',
+    type : 'General insurance',
+    desc: 'Insure now and get coverage of upto 5,00,000. Policy does not cover against existing health issues'
+  },
+  {
+    pname : "Corona Kavach",
+    id : 4,
+    type : 'Immediate Activation',
+    img : './src/cokavach.jpg',
+    desc: 'Get upto 8,00,000 in case of hospitalisation. Active from next day.'
+  },
+  {
+    pname : 'Arogya Sanjeevani',
+    id : 5,
+    type : 'The All in One Policy',
+    img : './src/Arogya-Sanjeevani-policy.jpg',
+    desc: 'Get insurance cover for all issues. For existing issues premium will be higher.'
+  },
+]
+
+
+
 app.set('view engine', 'ejs');
 
 /* middleware */
@@ -63,7 +110,7 @@ var con = mysql.createConnection({
       else if(pass == results[0].password){
         console.log("Success!");
         con.query("select * from customer where customer_id="+userid,function(err,results,fields){
-          res.render('customer',{uid : userid, details: results[0] }); // renders username and password and calls customer dashboard
+          res.render('customer',{uid : userid, details: results[0], policyTypes }); // renders username and password and calls customer dashboard
         });
       }
       else{
@@ -78,13 +125,22 @@ var con = mysql.createConnection({
   app.get("/signup",(req,res)=>{
     res.render("signup");
   });
-
+  /* */
 
   /*profile*/
   app.get('/profile',(req,res)=>{
     con.query("select * from customer where customer_id="+userid,function(err,results,fields){
-      res.render('profile',{uid : userid, details: results[0], pdet : pdetails });
+      con.query("select * from policy where policy_no = "+results[0].policy_no,function(error,result,field){
+        pdetails = result[0];
+        console.log(typeof(pdetails.maturity_date));
+        res.render('profile',{uid : userid, details: results[0], pdet : pdetails });
+      })
     });
+  });
+
+  /*Customer call */
+  app.get("/customer",(req,res)=>{
+    res.render("customer",{uid : userid, details: details, policyTypes });
   });
 
   /*admin call*/
@@ -113,6 +169,13 @@ var con = mysql.createConnection({
       console.log(results);
     })
     console.log(name,req.body.email,req.body.phone,req.body.date,req.body.msg);
+  });
+
+  /*Agent call */
+  app.get('/schedulecall',(req,res)=>{
+
+
+    res.render("schedulecall");
   });
 
 
